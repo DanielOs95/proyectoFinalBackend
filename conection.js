3b668e624b2cba09989445ce0b5ac93f7828e567
Daniel <espartan.95.os@gmail.com>
2024-10-20 20:40:17 -0500

    seguncoCommit

--- conection.js
+++ conection.js
@@ -1,10 +1,53 @@
 const express = require('express')
 const mysql = require('mysql2')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+
+app.get('/backend_final', (req, res) => {
+
+    const conexion = mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password:'Marisol/MaMa-1995',
+        database:'backend_final'
+    });
+    
+    conexion.connect((error) => {
+        if (error) {
+            console.error('Error al conectar a mysql:', error.message)
+        }
+        console.log('conexion exitosa')
+    });
+    
+    let datoSql = 'SELECT * FROM productos';
+    conexion.query(datoSql, (err, results) => {
+        if (err) {
+           console.error('error', err.message); 
+        }
+    
+        res.json(results)
+     conexion.end()
+    });
+    
+});
+
+
+
+
+app.listen('3001', () => {
+    console.log('api corriendo en http://localhost:3001/backend_final')
+});
+
+
+/*const express = require('express')
+const mysql = require('mysql2')
 const app = express()
 
 
 app.get('/libros_project/:libros', (req, res) => {
-    //res.send({message: hola ${req.params.libros}})
+    
 
     const conexion = mysql.createConnection({
         host:'localhost',
@@ -25,16 +68,15 @@ app.get('/libros_project/:libros', (req, res) => {
         }
     
         res.json(results)
-        //console.log('Datos', results);
-    
-    });
+        
     conexion.end()
+    });
 })
 
 
 
 app.listen('3001', () => {
-    console.log('api corriendo en http://localhost:3001/libros_project/:libros')
-})
+    console.log('api corriendo en http://localhost:3001/libros_project')
+})*/
 
 
--- frontend/script.js
+++ frontend/script.js
@@ -1,23 +1,108 @@
-//const { response } = require("express")
+const input = document.querySelector('input');
+const addBtn = document.querySelector('.btn-add');
+const ul = document.querySelector('ul');
+const empty = document.querySelector('.empty');
 
-//const { error } = require("console");
 
-//const { response } = require("express")
+addBtn.addEventListener('click', (e) => {
+    e.preventDefault();
 
-let btnShow = document.getElementById('btnMostrar')
+    const text =input.value.trim();
+
+    if (text !== "") {
+        const li = document.createElement('li');
+        const p = document.createElement('p');
+        p.textContent = text;
+
+        li.appendChild(p);
+        li.appendChild(addDeleteBtn());
+        ul.appendChild(li)
+
+        input.value = "";
+        empty.style.display = "none";
+    }
+});
+
+const addDeleteBtn = () => {
+    const deleteBtn = document.createElement('button');
+
+    deleteBtn.textContent = "X";
+    deleteBtn.className = 'btn-delete';
+
+    deleteBtn.addEventListener('click', (e) => {
+        const item = e.target.parentElement;
+
+        ul.removeChild(item);
+
+        const items = document.querySelectorAll('li');
+
+        if (items.length === 0) {
+            empty.style.display = "block";
+        }
+    });
+
+    return deleteBtn;
+};
+
+let btnShow = document.getElementById('lista')
 
 btnShow.addEventListener('click', () => {
-    fetch('http://localhost:3001/libros_project/:libros')
+    fetch('http://localhost:3001/backend_final')
     .then(response => response.json())
     .then(data => {
         const showList = document.getElementById('mostrar');
-        data.results.forEach(libros => {
-            const showDatos = document.createElement('tr');
-            showDatos.textContent = libros.title;
+        data.forEach(productos => {
+            const showDatos = document.createElement('li');
+            showDatos.textContent = productos.nombre;
             showList.appendChild(showDatos);
+
+          showList.appendChild(showDatos);
         });
+       
     })
     .catch(error => {
         console.error('Error:', error);
     });
 })
+
+
+
+
+/*let btnShow = document.getElementById('btnLibros')
+
+btnShow.addEventListener('click', () => {
+    fetch('http://localhost:3001/libros_project')
+    .then(response => response.json())
+    .then(data => {
+        const showList = document.getElementById('mostrar');
+        data.forEach(libros => {
+            const showDatos = document.createElement('tr');
+
+
+
+            const showTitulo = document.createElement('th')
+            showDatos.textContent = libros.titulo;
+            showList.appendChild(showTitulo);*/
+
+            /*const showPaginas = document.createElement('th')
+            showDatos.textContent = libros.paginas;
+            showList.appendChild(showPaginas);
+
+            const showPublicacion = document.createElement('th')
+            showDatos.textContent = libros.publicacion_fecha;
+            showList.appendChild(showPublicacion);
+
+            const showEditorial = document.createElement('th')
+            showDatos.textContent = libros.editorial;
+            showList.appendChild(showEditorial);*/
+
+
+            /*showList.appendChild(showDatos);
+        });
+       
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+})*/
+
